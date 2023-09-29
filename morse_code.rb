697af36787654610d4499a60267264ad78cb8888
Wineshuga <uzochukwuwinnie@gmail.com>
2023-09-29 21:33:16 +0100

    Fix linters

--- morse_code.rb
+++ morse_code.rb
@@ -1,25 +1,23 @@
 def decode_char(str)
   morse_letters = {
-  'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..',
-  'E' => '.', 'F' => '..-.', 'G' => '--.', 'H' => '....',
-  'I' => '..', 'J' => '.---', 'K' => '-.-.', 'L' => '.-..',
-  'M' => '--', 'N' => '-.', 'O' => '---', 'P' => '.--.',
-  'Q' => '--.-', 'R' => '.-.', 'S' => '...', 'T' => '-',
-  'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-',
-  'Y' => '-.--', 'Z' => '--..'
-}
+    'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..',
+    'E' => '.', 'F' => '..-.', 'G' => '--.', 'H' => '....',
+    'I' => '..', 'J' => '.---', 'K' => '-.-.', 'L' => '.-..',
+    'M' => '--', 'N' => '-.', 'O' => '---', 'P' => '.--.',
+    'Q' => '--.-', 'R' => '.-.', 'S' => '...', 'T' => '-',
+    'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-',
+    'Y' => '-.--', 'Z' => '--..'
+  }
 
-morse_letters.select {|key, value| value == str}.keys[0]
-
+  morse_letters.select { |_key, value| value == str }.keys[0]
 end
-
 
 def decode_word(str)
   split_arr = str.split
   word = ''
   split_arr.each do |char|
     word += decode_char(char)
-  end 
+  end
   word
 end
 
@@ -28,7 +26,7 @@ def decode(str)
   sentence = ''
   split_arr.each_with_index do |word, index|
     sentence += decode_word(word)
-    sentence += ' ' if index < split_arr.length-1
-  end 
+    sentence += ' ' if index < split_arr.length - 1
+  end
   sentence
 end
